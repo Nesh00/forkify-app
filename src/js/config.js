f880976df32e63f40bc2ce824d7fde04c247a64f
nenad <c.nenad00@gmail.com>
2021-05-29 15:12:33 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '375e337f-5a1a-4909-a04f-466924873e1a';
+export const MODAL_CLOSE_SEC = 2.5;
